7a06f6a5162ed084d105d6e5f1d00c06a117f8c9
Arslan <alijansoomro0@gmail.com>
2022-03-31 10:15:56 +0500

    Add Error Catching @ user post/get signin route

--- routes/user.route.js
+++ routes/user.route.js
@@ -21,12 +21,10 @@ router.post("/signup", async (req, res) => {
         res.status(200).json(newUser);
       } catch (err) {
         console.log("Error@Post-User: " + err);
-        res
-          .status(500)
-          .json({
-            message:
-              "The Server has faced some problem while processing your request",
-          });
+        res.status(500).json({
+          message:
+            "The Server has faced some problem while processing your request",
+        });
       }
     } else {
       res.status(400).json({ message: "One of the entries are missing" });
@@ -39,77 +37,96 @@ router.post("/signup", async (req, res) => {
   });
 });
 
-//To login
-//TODO Add login via token possible
+//To login via username and password
 router.post("/signin", async (req, res) => {
-  if (reqUname != undefined && reqPass != undefined) {
-    const reqUname = req.body.username?.toLowerCase(); //Convert to lowercase to search in the database
-    const reqPass = req.body.password;
+  try {
+    if (reqUname != undefined && reqPass != undefined) {
+      const reqUname = req.body.username?.toLowerCase(); //Convert to lowercase to search in the database
+      const reqPass = req.body.password;
 
-    //if Submitted Username and Password are not empty
+      //if Submitted Username and Password are not empty
 
-    //Find user with submitted username
-    let userData = await UserModel.find({ username: reqUname });
+      //Find user with submitted username
+      let userData = await UserModel.find({ username: reqUname });
 
-    //If user exist
-    if (userData.length > 0) {
-      userData = userData[0];
-    } else {
-      //else send an error response
-      res.status(400).json({ message: "Invalid Username" });
-      return;
+      //If user exist
+      if (userData.length > 0) {
+        userData = userData[0];
+      } else {
+        //else send an error response
+        res.status(400).json({ message: "Invalid Username" });
+        return;
+      }
+
+      //Check if password is correct
+      if (
+        userData?.password &&
+        (await bcrypt.compare(reqPass, userData.password))
+      ) {
+        //if user is found and password matches
+        //sign id and username to generate token
+        const accessToken = jwt.sign(
+          { _id: userData._id, username: userData.username },
+          process.env.JWT_SECRET,
+          { expiresIn: "24h" }
+        );
+        res
+          .status(200)
+          .json({ data: { token: accessToken }, message: "Login Successful" }); //submit back the token
+        return;
+      } else {
+        res.status(400).json({ message: "Invalid Password" });
+        return;
+      }
     }
+    res.status(400).json({ message: "One of the fields are missing" });
+  } catch (err) {
+    console.log("Error@User-POST-Signin: " + err.message);
+    res
+      .json(500)
+      .status({
+        message: "Server has faced some issue while processing your request",
+      });
+  }
+});
 
-    //Check if password is correct
-    if (
-      userData?.password &&
-      (await bcrypt.compare(reqPass, userData.password))
-    ) {
-      //if user is found and password matches
-      //sign id and username to generate token
-      const accessToken = jwt.sign(
-        { _id: userData._id, username: userData.username },
-        process.env.JWT_SECRET,
-        { expiresIn: "24h" }
+//To login via token
+router.get("/signin", async (req, res) => {
+  try {
+    const token = req.header("token");
+
+    //If We have received a token
+    if (token != undefined) {
+      //Verify token and extract its data
+      let tokenData = verifyToken(token);
+
+      //If token is invalid, send back an error message
+      if (tokenData === undefined) {
+        res.status(401).json({ message: "Invalid Token" });
+        return;
+      }
+
+      //If token is valid, Return back all user data except password
+      let userData = await UserModel.findOne(
+        { username: tokenData.username.toLowerCase() },
+        { password: 0 }
       );
       res
         .status(200)
-        .json({ data: { token: accessToken }, message: "Login Successful" }); //submit back the token
-      return;
-    } else {
-      res.status(400).json({ message: "Invalid Password" });
-      return;
-    }
-  }
-  res.status(400).json({ message: "One of the fields are missing" });
-});
+        .json({
+          message: "Token Authenticated, Access Granted",
+          data: userData,
+        });
 
-router.get("/signin", async (req, res) => {
-  const token = req.header("token");
-
-  //If We have received a token
-  if (token != undefined) {
-    //Verify token and extract its data
-    let tokenData = verifyToken(token);
-
-    //If token is invalid, send back an error message
-    if (tokenData === undefined) {
-      res.status(401).json({ message: "Invalid Token" });
       return;
     }
-
-    //If token is valid, Return back all user data except password
-    let userData = await UserModel.findOne(
-      { username: tokenData.username.toLowerCase() },
-      { password: 0 }
-    );
-    res
-      .status(200)
-      .json({ message: "Token Authenticated, Access Granted", data: userData });
-
-    return;
+    res.status(401).json({ message: "Token Not Found" });
+  } catch (err) {
+    console.log("Error@User-GET-Signin: " + err.message);
+    res.status(500).json({
+      message: "Server has faced an issue while processing your request",
+    });
   }
-  res.status(401).json({ message: "Token Not Found" });
 });
 
 router.get("/", async (req, res) => {
